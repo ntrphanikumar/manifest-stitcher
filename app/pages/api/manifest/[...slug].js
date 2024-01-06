0e20b9e848f733ab20ee5068d9ce457e6855de1e
N T R Phani Kumar <ntrphanikumar@yupptv.com>
2024-01-06 11:10:41 +0530

    Added cors

--- app/pages/api/manifest/[...slug].js
+++ app/pages/api/manifest/[...slug].js
@@ -2,7 +2,21 @@ import {channelPlaylist, fetchPlaylist} from "./download"
 
 const MainManifest = '#EXTM3U\n#EXT-X-VERSION:4\n#EXT-X-INDEPENDENT-SEGMENTS\n#EXT-X-STREAM-INF:BANDWIDTH=1561904,AVERAGE-BANDWIDTH=1161845,FRAME-RATE=25.000,CODECS="avc1.64001E,mp4a.40.2",RESOLUTION=640x360\nbitrate0.m3u8\n#EXT-X-STREAM-INF:BANDWIDTH=3738192,AVERAGE-BANDWIDTH=2719428,FRAME-RATE=25.000,CODECS="avc1.64001F,mp4a.40.2",RESOLUTION=1280x720\nbitrate1.m3u8\n#EXT-X-STREAM-INF:BANDWIDTH=5162480,AVERAGE-BANDWIDTH=3733809,FRAME-RATE=25.000,CODECS="avc1.640028,mp4a.40.2",RESOLUTION=1920x1080\nbitrate2.m3u8'
 
-export default async function handler(req, res) {
+const enableCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.setHeader( 'Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+  // specific logic for the preflight request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  return await fn(req, res)
+}
+
+const action = async (req, res) => {
   const { slug } = req.query
   const channel = slug[0]
   const file = slug[1]
@@ -11,7 +25,11 @@ export default async function handler(req, res) {
   else if(file === 'bitrate0.m3u8') res.send(await childManifest(channel, '640'))
   else if(file === 'bitrate1.m3u8') res.send(await childManifest(channel, '1280'))
   else if(file === 'bitrate2.m3u8') res.send(await childManifest(channel, '1920'))
-  else res.end(`Post: ${slug.join(', ')}`)
+  else res.status(404).send('Not found')
+}
+
+export default async function handler(req, res) {
+  enableCors(action)(req, res)
 }
 
 async function childManifest(channel, width) {
