179568420732b1b51902c8ae1a13ec1d4990ac5e
N T R Phani Kumar <ntrphanikumar@yupptv.com>
2024-01-08 11:57:27 +0530

    Validating chunks

--- app/pages/api/manifest/validate.js
+++ app/pages/api/manifest/validate.js
@@ -8,20 +8,17 @@ export default async (req, res) => {
     res.status(200).json({message: 'Validation initiated'});
 };
 
-function fullUri(parentUrl, uri, isSegment) {
+function fullUri(parentUrl, uri) {
     const mUrl = new URL(parentUrl)
     const mPrefix = mUrl.origin + mUrl.pathname.replace(mUrl.pathname.split("/").slice(-1)[0], '')
     let fUrl = (uri.startsWith("/"))?mUrl:(uri.startsWith("http")?'':mPrefix)+uri
-    if(isSegment && process.env.SEGMENTS_HOST) {
-        fUrl = new URL(fUrl)
-        return process.env.SEGMENTS_HOST+fUrl.pathname+fUrl.search
-    } else return fUrl
+    return fUrl
 }
 
 async function downloadChildManifests(variants, payload) {
     const result = await Promise.all(variants.map(v => downloadManifest(fullUri(payload.playbackUrl, v.uri), payload)))
     const failure = result.map((r,idx) => {
-        if(r.error) r.uri = variants[idx].uri
+        r.uri = fullUri(payload.playbackUrl, variants[idx].uri)
         return r
     }).filter(r => r.failed === true).reduce((a,b)=>{
         return {
@@ -39,8 +36,8 @@ async function downloadManifest(url, payload) {
         console.log(url)
         return await axios.get(url).then(async response => {
             return HLS.parse(response.data)
-        }).catch(error => {
-            return error(url, error.toString(), payload)
+        }).catch(e => {
+            return error(url, e.toString(), payload)
         })
     } catch(e) {
         return error(url, e.toString(), payload)
@@ -58,7 +55,29 @@ function validateDuration(manifest, payload) {
 }
 
 async function validateChunks(manifest, payload) {
-    return 
+    async function downloadChunk(segment) {
+        const chunkUrl = fullUri(manifest.uri || payload.playbackUrl, segment.uri)
+        console.log(chunkUrl)
+        return await axios.get(chunkUrl).then(async response => {
+            return {url: segment.uri, failed: false}
+        }).catch(e => {
+            return error(segment.uri, e.toString(), payload)
+        })
+    }
+
+    const groupedSegments = manifest.segments.reduce((resultArray, item, index) => { 
+        const chunkIndex = Math.floor(index/25)
+        if(!resultArray[chunkIndex])  resultArray[chunkIndex] = []
+        resultArray[chunkIndex].push(item)
+        return resultArray
+    }, [])
+
+    let gsResult = []
+    for(var idx=0;idx<groupedSegments.length;idx++) {
+        gsResult[idx] = (await Promise.all(groupedSegments[idx].map(s => downloadChunk(s)))).filter(r => r.failed === true).reduce(reduceR, {})
+    }
+    const result = gsResult.filter(r => r.failed === true).reduce(reduceA, {});
+    if(result.failed === true) return result
 }
 
 function error(url, reason, payload) {
@@ -66,9 +85,20 @@ function error(url, reason, payload) {
         failed: true,
         failedUrl: url,
         reason: reason,
-        playbackUrl: payload.playbackUrl,
-        chunksValidated: false
+        playbackUrl: payload.playbackUrl
     }
+}
+
+function reduceR(a,b) {
+    b.reason = (a.reason || []).concat([{url: b.failedUrl, reason: b.reason}])
+    b.failedUrl = (a.failedUrl || []).concat([b.failedUrl])
+    return b
+}
+
+function reduceA(a,b) {
+    b.reason = (a.reason || []).concat(b.reason)
+    b.failedUrl = (a.failedUrl || []).concat(a.failedUrl)
+    return b
 }
 
 async function validate(payload) {
@@ -82,8 +112,8 @@ async function validate(payload) {
                 const durationFailed = validateDuration(childManifests[0], payload)
                 if(durationFailed) return durationFailed
                 if(payload.validateChunks) {
-                    const result = await Promise.all(childManifests.map(m => validateChunks(m, payload)))
-                    console.log(result)
+                    const result = (await Promise.all(childManifests.map(m => validateChunks(m, payload)))).filter(r => r !== undefined).reduce(reduceA, {})
+                    if(result.failed === true) return result
                 }
             } else if(payload.validateChunks) return await validateChunks(manifest, payload)
         } catch (e){
